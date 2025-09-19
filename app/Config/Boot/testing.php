eabe0d74d89645ac387f66dc17846d61f4381cec
Renzo Castillo <renzo12castillo@gmail.com>
2025-09-19 16:38:53 -0600

    Se pasa el codigo a modo produccion

--- app/Config/Boot/testing.php
+++ app/Config/Boot/testing.php
@@ -15,7 +15,7 @@
  | painful debugging.
  */
 error_reporting(E_ALL);
-ini_set('display_errors', '1');
+ini_set('display_errors', '0');
 
 /*
  |--------------------------------------------------------------------------
